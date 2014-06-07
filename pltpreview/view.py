5056704f71ed6b871adb20528a04ddc81bae3ddb
Tomas Farago <sensej007@email.cz>
2014-06-07 09:53:49 +0200

    Add code

---
+++ pltpreview/view.py
@@ -0,0 +1,26 @@
+"""Convenience functions for matplotlib plotting and image viewing."""
+import numpy as np
+from matplotlib import pyplot as plt
+
+
+def show(image, blocking=False, **kwargs):
+    """Show *image*. If *blocking* is False the call is nonblocking.
+    *kwargs* are passed to matplotlib's ``imshow`` function. This command
+    always creates a new figure. Returns matplotlib's ``AxesImage``.
+    """
+    plt.figure()
+    mpl_image = plt.imshow(image, **kwargs)
+    plt.colorbar(ticks=np.linspace(image.min(), image.max(), 8))
+    plt.show(blocking)
+
+    return mpl_image
+
+
+def plot(*args, **kwargs):
+    """Plot using matplotlib's ``plot`` function. Pass it *args* and *kwargs*.
+    *kwargs* are infected with *blocking* and if False or not specified,
+    the call is nonblocking.
+    """
+    blocking = False if 'blocking' not in kwargs else kwargs.pop('blocking')
+    plt.plot(*args, **kwargs)
+    plt.show(blocking)
---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+
+setup(name='pltpreview',
+      version='0.1',
+      license='MIT',
+      description='Convenience funcitons for plotting and image viewing with matplotlib',
+      author='Tomas Farago',
+      author_email='sensej007@email.cz',
+      packages=['pltpreview'],
+      install_requires=['numpy', 'matplotlib'],
+     )
